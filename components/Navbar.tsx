3bbab70284e22a5f2ca9b5ba4e2671feb2997222
Eccentric19Shivam <singhshivamhp16@gmail.com>
2024-08-01 16:57:57 +0530

    implemented recordings feature and nearly got my project crashed but fixed it

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,28 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import MobileNav from "./MobileNav";
-import { ClerkProvider, SignedIn, UserButton } from "@clerk/nextjs";
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
-    <nav className=" flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
           width={32}
           height={32}
-          alt="logo"
+          alt="yoom logo"
           className="max-sm:size-10"
         />
-        <p className="font-extrabold text-[26px] text-white max-sm:hidden">
-          Voom
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          YOOM
         </p>
       </Link>
-
       <div className="flex-between gap-5">
-        {/* <ClerkProvider>
-      <SignedIn>
-          <UserButton afterSignOutUrl="/sign-in"/>
-      </SignedIn>
-        </ClerkProvider> */}
         <SignedIn>
-        <UserButton afterSignOutUrl="/sign-in"/>
-      </SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn>
 
         <MobileNav />
       </div>
